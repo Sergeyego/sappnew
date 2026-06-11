ae2a31e4369ef3e37e1dce2227674f4da736bd67
sergen <es@szsm-mail.ru>
2026-06-11 10:11:01 +0300

    fix upd info

--- index.js
+++ index.js
@@ -22,8 +22,10 @@ global.tables = new Map();
 global.rels = new Map();
 
 autorest.updData()
-    .then(() => {
-        console.log("Данные успешно получены.");
+    .then((inf) => {
+        if (inf.ok){
+            console.log("Данные успешно получены.");
+        }
     })
     .catch((error) => {
         console.log("Не удалось обновить данные. " + error.message);
